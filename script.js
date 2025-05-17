52750dc42eeeeae7993d175f99a5d4103e9152aa
riez1623 <riezmatthieujuly@gmail.com>
2025-05-17 14:39:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,79 @@ const nextPageBtn = document.getElementById('nextPage');
 const bookPage = document.getElementById('bookPage');
 const transitionOverlay = document.getElementById('transitionOverlay');
 
-const story = `It all started when I was just laying on my bed... [full story continued here] ... I'd still choose you, in every world, in every universe, every multiverse, I'd still choose you.`;
+const story = `It all started when I was just laying on my bed, I was so bored, my friends were all busy, the others were doing school activities while others were just playing. I was really bored and I thought I could talk to a new person or a new friend, I always thought having new friends was always a good idea.
+
+I then asked my friend named Ahron if he had some friends that I could talk to, I also told him that I wanted to talk to a girl haha, well he told me about this girl, her name was Ciara. Of course I started searching for her social, then I finally found it, I started talking to her and wow. She's so kind, sweet and friendly.
+
+We started talking, asking each other's hobbies, we were getting to know each other already. While we were talking, I think I was already feeling comfortable around her. She's just so fun to talk to.
+
+After a few more weeks, we've gotten to know each other very well. We knew each other's birthdays, favorite food, favorite color. We kept on talking like there was no tomorrow.
+
+After a few more months, I figured that I had feelings for Ciara. I confessed. She told me that she likes me as well, so I courted her.
+
+My friends thought me and Ciara should meet up. I really did think that it was a bad idea, I mean we only started talking for a few months and now we're gonna meet up?
+
+I was scared. I'm scared because I know myself that I'm ugly. I told her I didn't wanna meet up, I told her I was scared, I was scared of judgment. I was scared she's gonna leave me. She then told me "Don't worry, I don't care about looks, all I want is your personality". She really reassured me, she made me know that I had nothing to be scared of.
+
+The night before we met, I was excited and scared at the same time. After school, me and my other friends went to the meet-up place, it was very traffic, Ciara and Ahron were waiting for us at a convenience store. I arrived late because of the stupid traffic, then yeah, I saw her, Ciara was just sitting on a bench and I just stood there, looking at her gorgeous, pretty, perfect face.
+
+When none of us were making a move, Ahron pushed me in front of Ciara. I fell on the same bench where Ciara was sitting. I was standing up and was about to sit down as well, but when I was about to sit down, Ciara hugged me, I hugged her back.
+
+I was nervous since that was the first ever girl that I've hugged.
+
+We talked there for minutes, talking about how we were, how was school and stuff. After a few minutes she randomly gave me her hair tie, she said "Take good care of it, I'll give that to you so that whenever you miss me just hold on to that hair tie". I was so glad and hugged her.
+
+After that, we went inside the convenience store, I was still shy towards her so I just talked to Ahron while we were inside, then Ahron asked me "Why aren't you talking to her bro?" I didn't wanna answer, I can't just tell the reason why, Ciara was literally behind me. While I was just laughing it off, Ciara hugged me from behind, I felt butterflies, I was really blushing that time, no lies.
+
+After we bought some snacks, we went outside and looked for a spot to hangout. While we were crossing the road, Ciara held my hand, I definitely fell even harder towards her. It was my first time holding a woman's hands. Her hands were soft, I didn't wanna let go.
+
+We finally arrived and ate all the snacks we bought. After a few minutes, Ahron had to go, leaving me and Ciara alone, we said goodbye to Ahron, waving goodbye while he walks away.
+
+Me and Ciara talked about some literally random stuff. We sat there eating, talking for hours till sunset, we watched the sunset, we were holding hands, sitting, leaning onto each other. It was the best day in my entire life. After a few more minutes, Ciara had to go since she needs to get home before dark.
+
+I led her to the bus station, I hugged her and she got into the bus, I waved her goodbye as the bus leaves. I went home as well since it was getting dark. While I was riding the bus, I can't help but smile and think about the things that happened that day.
+
+When I got home, I texted her, asking if she got home safe. She responded quickly, saying "Yes". We talked for minutes and we went to sleep.
+
+I really had a good sleep that night, it was so good that I even dreamed about what happened that day. I woke up happy, I wished all of that happened again.
+
+I wanted to meet her again, but she had strict parents, she just found a good excuse to meet up with me and said she can't meet me again, she told me that we can meet up maybe in a few weeks.
+
+It didn't really matter that much, I was happy I met her and could still talk to her, even though it's just through texting.
+
+After a few more months, we really had a healthy relationship even though we had small problems, we both kept on finding a way to solve the problems we had.
+
+One day, she said something to me. And I wasn't really happy nor mad about it. She told me that her "old crush" texted her and asked her if she wants to meet up. She told me the story about her and her old crush.
+
+That guy was a grade 3 classmate of hers, and they liked each other, but her mom found out and Ciara transferred to another school, they never talked since then.
+
+I told Ciara that it was fine to meet him, I didn't really mind it, although I was really feeling jealous, but I just played some games to try and forget it.
+
+A few days after that, she became more distant towards me, she became cold, frozen cold. I was starting to feel like the "spark" is vanishing.
+
+After a few weeks, she told me that she got back with that guy. I was really devastated. I cried all night, thinking where I went wrong, thinking what I did to deserve it, thinking where did I go wrong?
+
+A few days later, Ciara blocked me on all my socials. I wanted to end myself.
+
+Then,
+
+I saw that she unblocked me, and I said I still love you thinking she wouldn't respond. But she did. March 28, I saw that she unblocked me, and I said I still love you thinking she wouldn't respond. But she did.
+
+We talked for a few minutes. I asked her if I could meet her one last time, knowing she wouldn't agree. But she did?
+
+Then the day that we would meet up, came. I saw her from afar and walked towards her, I wanted to hug her but I was so shy because Ahron was there. He asked me if I wanted to say something but I was just so speechless.
+
+But when he left, Ciara told me she couldn't stay too long. So when she was about to leave I hugged her, tight. I didn't wanna let go, she hugged me too, saying it's okay.
+
+Then yeah, a lot of people saw us hugging, we were in public and those people were like saying that we were cute, but I was just about to cry.
+
+When she left, I was sobbing but I couldn't do anything. I regret not saying anything while I was there with her. And yeah after that day, we didn't talk anymore.
+
+Maybe she's the right person but it's just the wrong time. Or maybe we weren't meant to be at all.
+
+First love has to be the worst, but if I die and get to live again, I'd still choose you.
+
+I'd still choose you, in every world, in every universe, every multiverse, I'd still choose you.`;
 
 let currentPage = 0;
 let pageArray = [];
@@ -56,10 +128,11 @@ function showTransition(callback) {
 function setupBook(device) {
   const sentences = story.split(/(?<=\.)\s+/);
   let sentencesPerPage = device === 'phone' ? 3 : device === 'tablet' ? 2 : 3;
-  let pages = device === 'phone' ? 1 : 2;
+  let totalSentences = sentences.length;
+  let totalPages = Math.ceil(totalSentences / sentencesPerPage);
 
   pageArray = [];
-  for (let i = 0; i < pages; i++) {
+  for (let i = 0; i < totalPages; i++) {
     const pageText = sentences.slice(i * sentencesPerPage, (i + 1) * sentencesPerPage).join(' ');
     pageArray.push(pageText);
   }
